e5efa3cb388eeba0f41c7d4ce0d9a835ec35e578
jetfuzz <1jsf23@gmail.com>
2025-05-03 15:46:55 -0500

    create gameboard object

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("test")
+const gameboard = (function () {
+    let board = [["~", "~", "~",], 
+                 ["~", "~", "~"], 
+                 ["~", "~", "~"]];
+
+    function display() {
+        for (let row of board) {
+            const rowString = row.join(" ")
+            console.log(rowString);
+        }
+    }
+
+    function change(row, column, value) {
+        if (board[row][column] = "~") {
+            board[row][column] = value
+        } else {
+            console.log("please enter a value space")
+        }
+    }
+
+    return {display, change}
+})();
+
+gameboard.display();
+gameboard.change(0, 0, "X");
+gameboard.change(1, 1, "O");
+gameboard.change(2, 1, "X");
+console.log("")
+gameboard.display();
+
+function Player () {
+    
+}
+
+
+function Game () {
+
+}
+
+function displayController () {
+
+}
+
