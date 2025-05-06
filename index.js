3627565754ff9d8e761324a2e139e4faca134b04
jetfuzz <1jsf23@gmail.com>
2025-05-06 11:32:11 -0500

    Create gameboard functions for console version

--- index.js
+++ index.js
@@ -5,36 +5,92 @@ const gameboard = (function () {
 
     function display() {
         for (let row of board) {
-            const rowString = row.join(" ")
+            const rowString = row.join(" ");
             console.log(rowString);
         }
     }
 
-    function change(row, column, value) {
-        if (board[row][column] = "~") {
-            board[row][column] = value
-        } else {
-            console.log("please enter a value space")
+    function place(row, column, value) {
+
+        board[row][column] = value;
+
+        display();
+        checkWin();
+        checkDraw();
+    }
+
+    function checkWin () {
+        for (let i = 0; i < 3; i++) {
+            let rowSum = "";
+            for (let j = 0; j < 3; j++) {
+                rowSum += board[i][j];
+            }
+            if (rowSum === "XXX") {
+                alert("player 1 wins!");
+            } else if (rowSum === "OOO") {
+                alert("player 2 wins!");
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            let colSum = "";
+            for (let j = 0; j < 3; j++) {
+                colSum += board[j][i];
+            }
+            if (colSum === "XXX") {
+                alert("player 1 wins!");
+            } else if (colSum === "OOO") {
+                alert("player 2 wins!");
+            }
+        }
+        if (board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X") {
+            alert("player 1 wins!");
+        } else if (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O") {
+            alert("player 2 wins!");
+        }
+        if (board[2][0] === "X" && board[1][1] === "X" && board[0][2] === "X") {
+            alert("player 1 wins!");
+        } else if (board[2][0] === "O" && board[1][1] === "O" && board[0][2] === "O") {
+            alert("player 2 wins!");
         }
     }
 
-    return {display, change}
+    function checkDraw() {
+        let emptySpace = 9;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j <3; j++) {
+                if (board[i][j] != "~") {
+                    emptySpace--;
+                    console.log(emptySpace);
+                }
+                if (emptySpace === 0) {
+                    alert("its a draw!")
+                }
+            }
+        }
+
+    }
+
+    return {display, place, checkWin, checkDraw};
 })();
 
-gameboard.display();
-gameboard.change(0, 0, "X");
-gameboard.change(1, 1, "O");
-gameboard.change(2, 1, "X");
-console.log("")
-gameboard.display();
 
-function Player () {
-    
+function Player (n, m) {
+    let name = n;
+    let marker = m;
+
+    function move () {
+        let row = prompt("Enter row:");
+        let column = prompt("Enter column:");
+        gameboard.place(row - 1, column - 1, marker);
+        gameboard.display();;
+    }
+
+    return {name, move};
 }
 
 
 function Game () {
-
+    
 }
 
 function displayController () {
